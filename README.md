29d23de825648804f203a567883bc671059f6937
Lalit Rajput <llmsjraj@gmail.com>
2023-11-22 18:43:00 +1100

    init angluar

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# KLPF
+# KUPF
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.2.
 
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../HousingLocation';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent,
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+
+export class HomeComponent {
+  filteredLocationList: HousingLocation[] = [];
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+  
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
